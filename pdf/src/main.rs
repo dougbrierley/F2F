948c2d6cd2217776fe6587987cc108a157037d08
Fraser Rennie <fjrennie1@outlook.com>
2024-02-14 23:39:24 +0100

    Order pdf

--- pdf/src/main.rs
+++ pdf/src/main.rs
@@ -6,6 +6,8 @@ use clap::Parser;
 use printpdf::*;
 use std::fs::File;
 use std::io::BufWriter;
+use std::io::Write;
+use termcolor::{ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 struct GrowerItem {
     grower: String,
@@ -48,14 +50,13 @@ impl std::fmt::Display for GrowerItemOrders<'_> {
         Ok(())
     }
 }
-
+#[derive(Debug)]
 struct Order {
     buyer: String,
-    lines: Vec<OrderLine>,
+    lines: std::collections::HashMap<String, Vec<OrderLine>>,
 }
-
+#[derive(Debug)]
 struct OrderLine {
-    grower: String,
     produce: String,
     variant: String,
     unit: String,
@@ -113,7 +114,7 @@ fn get_buyers(headers: &Vec<String>) -> (Vec<Buyer>, usize) {
 fn format_currency(pence: u32) -> String {
     let pounds = pence / 100;
     let pence = pence % 100;
-    format!("£{}.{}", pounds, pence)
+    format!("£{}.{:02}", pounds, pence)
 }
 
 fn add_order_line(
@@ -122,29 +123,60 @@ fn add_order_line(
     font: &IndirectFontRef,
     y_tracker_mm: f32,
 ) {
-    current_layer.set_font(&font, 12.0);
+    current_layer.set_font(&font, 10.0);
+
+    let font_size = 10.0;
 
     current_layer.begin_text_section();
-    current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
-    current_layer.write_text(&order_line.grower, &font);
+    current_layer.use_text(
+        &order_line.produce,
+        font_size,
+        Mm(10.0),
+        Mm(y_tracker_mm),
+        &font,
+    );
+    current_layer.use_text(
+        &order_line.variant,
+        font_size,
+        Mm(50.0),
+        Mm(y_tracker_mm),
+        &font,
+    );
+    current_layer.use_text(
+        &order_line.unit,
+        font_size,
+        Mm(120.0),
+        Mm(y_tracker_mm),
+        &font,
+    );
+    current_layer.use_text(
+        &order_line.qty.to_string(),
+        font_size,
+        Mm(140.0),
+        Mm(y_tracker_mm),
+        &font,
+    );
+    current_layer.use_text(
+        format_currency(order_line.price),
+        font_size,
+        Mm(160.0),
+        Mm(y_tracker_mm),
+        &font,
+    );
+    current_layer.use_text(
+        format_currency((order_line.qty * order_line.price as f32) as u32),
+        font_size,
+        Mm(180.0),
+        Mm(y_tracker_mm),
+        &font,
+    );
     current_layer.end_text_section();
+}
 
-    current_layer.begin_text_section();
-    current_layer.set_text_cursor(Mm(50.0), Mm(y_tracker_mm));
-    current_layer.write_text(&order_line.produce, &font);
-    current_layer.end_text_section();
-
-    current_layer.begin_text_section();
-    current_layer.set_text_cursor(Mm(90.0), Mm(y_tracker_mm));
-    current_layer.write_text(&order_line.variant, &font);
-
-    current_layer.set_text_cursor(Mm(130.0), Mm(y_tracker_mm));
-    current_layer.write_text(&order_line.qty.to_string(), &font);
-    current_layer.end_text_section();
-
+fn add_hr(current_layer: &PdfLayerReference, y_tracker_mm: f32, thickness: f32) {
     let outline_color_2 = Color::Greyscale(Greyscale::new(0.45, None));
     current_layer.set_outline_color(outline_color_2);
-    current_layer.set_outline_thickness(0.5);
+    current_layer.set_outline_thickness(thickness);
 
     let line2 = Line::from_iter(vec![
         (Point::new(Mm(10.0), Mm(y_tracker_mm - 1.2)), false),
@@ -155,26 +187,96 @@ fn add_order_line(
     current_layer.add_line(line2);
 }
 
-fn add_order_lines_to_pdf(current_layer: &PdfLayerReference, font: &IndirectFontRef, order_lines: &Vec<OrderLine>) {
+fn add_order_lines_to_pdf(
+    current_layer: &PdfLayerReference,
+    font: &IndirectFontRef,
+    order_lines: &std::collections::HashMap<String, Vec<OrderLine>>,
+    y_tracker_mm: &mut f32,
+) {
+    *y_tracker_mm -= 7.0;
 
-    let mut y_tracker_mm = 200.0;
-    for order_line in order_lines {
-        add_order_line(current_layer, order_line, font, y_tracker_mm);
-        y_tracker_mm -= 6.0;
+    for (k, v) in order_lines {
+        *y_tracker_mm -= 3.0;
+        current_layer.use_text(k, 12.0, Mm(10.0), Mm(*y_tracker_mm), &font);
+        current_layer.use_text(
+            format_currency(total_per_grower(v)),
+            12.0,
+            Mm(180.0),
+            Mm(*y_tracker_mm),
+            font,
+        );
+        *y_tracker_mm -= 1.0;
+        add_hr(current_layer, *y_tracker_mm, 0.5);
+        *y_tracker_mm -= 6.0;
+        for order_line in v {
+            add_order_line(current_layer, order_line, font, *y_tracker_mm);
+            *y_tracker_mm -= 6.0;
+        }
     }
 }
 
-    
+fn total_per_grower(lines: &Vec<OrderLine>) -> u32 {
+    lines.iter().map(|l| l.price * l.qty as u32).sum()
+}
+
+fn add_total(
+    current_layer: &PdfLayerReference,
+    font: &IndirectFontRef,
+    font_special: &IndirectFontRef,
+    y_tracker_mm: f32,
+    total: u32,
+) {
+    let font_size = 14.0;
+
+    add_hr(current_layer, y_tracker_mm, 1.0);
+
+    current_layer.begin_text_section();
+    current_layer.use_text(
+        "TOTAL",
+        font_size,
+        Mm(10.0),
+        Mm(y_tracker_mm - 8.0),
+        &font_special,
+    );
+    current_layer.use_text(
+        format_currency(total),
+        font_size,
+        Mm(180.0),
+        Mm(y_tracker_mm - 8.0),
+        &font,
+    );
+    current_layer.end_text_section();
+}
+
+fn total_per_order(order: &Order) -> u32 {
+    order.lines.values().map(|l| total_per_grower(l)).sum()
+}
+
+fn add_table_header(current_layer: &PdfLayerReference, font: &IndirectFontRef, y_tracker_mm: f32) {
+    let font_size = 12.0;
+
+    add_hr(current_layer, y_tracker_mm + 6.0, 1.0);
+
+    current_layer.begin_text_section();
+    current_layer.use_text("PRODUCE", font_size, Mm(10.0), Mm(y_tracker_mm), &font);
+    current_layer.use_text("DESCRIPTION", font_size, Mm(50.0), Mm(y_tracker_mm), &font);
+    current_layer.use_text("UNIT", font_size, Mm(120.0), Mm(y_tracker_mm), &font);
+    current_layer.use_text("QTY", font_size, Mm(140.0), Mm(y_tracker_mm), &font);
+    current_layer.use_text("PRICE", font_size, Mm(160.0), Mm(y_tracker_mm), &font);
+    current_layer.use_text("TOTAL", font_size, Mm(180.0), Mm(y_tracker_mm), &font);
+    current_layer.end_text_section();
+    add_hr(current_layer, y_tracker_mm, 1.0);
+}
+
 fn create_buyer_orders(orders: Vec<&Order>) {
     for order in orders {
         create_buyer_order(order)
     }
 }
 
-
 fn create_buyer_order(order: &Order) {
-    let (doc, page1, layer1) =
-        PdfDocument::new("PDF_Document_title", Mm(210.0), Mm(297.0), "Layer 1");
+    let pdf_title = format!("Order for {}", order.buyer);
+    let (doc, page1, layer1) = PdfDocument::new(pdf_title, Mm(210.0), Mm(297.0), "Layer 1");
     let current_layer = doc.get_page(page1).get_layer(layer1);
 
     let mut y_tracker_mm = 267.0;
@@ -185,41 +287,52 @@ fn create_buyer_order(order: &Order) {
     let normal_roboto = doc
         .add_external_font(File::open("assets/fonts/Roboto-Regular.ttf").unwrap())
         .unwrap();
+    let oswald = doc
+        .add_external_font(File::open("assets/fonts/Oswald-Medium.ttf").unwrap())
+        .unwrap();
 
     current_layer.begin_text_section();
 
-    current_layer.set_font(&normal_roboto, 46.0);
+    current_layer.set_font(&oswald, 46.0);
     current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
     current_layer.set_line_height(8.0);
     current_layer.set_word_spacing(0.0);
     current_layer.set_character_spacing(0.0);
 
-    current_layer.write_text("ORDER", &medium);
+    current_layer.write_text("ORDER", &oswald);
 
     current_layer.end_text_section();
 
     current_layer.begin_text_section();
     y_tracker_mm -= 18.0;
-    current_layer.set_font(&normal_roboto, 12.0);
-    current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
+    current_layer.set_font(&oswald, 12.0);
+    current_layer.set_text_cursor(Mm(140.0), Mm(y_tracker_mm));
     current_layer.set_fill_color(Color::Rgb(Rgb::new(0.0, 0.04, 0.0, None)));
 
     current_layer.set_line_height(14.0);
-    current_layer.write_text("Order Number", &normal_roboto);
-    current_layer.add_line_break();
-    current_layer.set_font(&normal_roboto, 10.0);
-    current_layer.write_text("00001", &normal_roboto);
-
+    current_layer.write_text("ORDER #", &oswald);
     current_layer.end_text_section();
 
     current_layer.begin_text_section();
-    current_layer.set_text_cursor(Mm(70.0), Mm(y_tracker_mm));
+    current_layer.set_text_cursor(Mm(180.0), Mm(y_tracker_mm));
+    current_layer.set_font(&normal_roboto, 10.0);
+    current_layer.write_text("F2F0601", &normal_roboto);
+
+    current_layer.end_text_section();
+
+    y_tracker_mm -= 6.0;
+
+    current_layer.begin_text_section();
+    current_layer.set_text_cursor(Mm(140.0), Mm(y_tracker_mm));
 
     let dt = Utc.with_ymd_and_hms(2024, 2, 20, 0, 0, 0).unwrap();
 
-    current_layer.set_font(&normal_roboto, 12.0);
-    current_layer.write_text("Date of Order", &normal_roboto);
-    current_layer.add_line_break();
+    current_layer.set_font(&oswald, 12.0);
+    current_layer.write_text("ORDER DATE", &oswald);
+    current_layer.end_text_section();
+
+    current_layer.begin_text_section();
+    current_layer.set_text_cursor(Mm(180.0), Mm(y_tracker_mm));
     current_layer.set_font(&normal_roboto, 10.0);
     current_layer.write_text(&dt.format("%d/%m/%Y").to_string(), &normal_roboto);
 
@@ -235,7 +348,7 @@ fn create_buyer_order(order: &Order) {
     }
 
     let buyer_details = BuyerDetails {
-        name: "Exeter College".to_string(),
+        name: order.buyer.clone(),
         address1: "Turl Street".to_string(),
         address2: None,
         city: "Oxford".to_string(),
@@ -245,10 +358,10 @@ fn create_buyer_order(order: &Order) {
 
     current_layer.begin_text_section();
 
-    y_tracker_mm -= 16.0;
-    current_layer.set_font(&normal_roboto, 14.0);
+    y_tracker_mm += 6.0;
+    current_layer.set_font(&oswald, 14.0);
     current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
-    current_layer.write_text("BILLED TO", &normal_roboto);
+    current_layer.write_text("DELIVER TO", &oswald);
 
     current_layer.end_text_section();
 
@@ -278,125 +391,23 @@ fn create_buyer_order(order: &Order) {
 
     current_layer.end_text_section();
 
-    add_order_lines_to_pdf(&current_layer, &normal_roboto, &order.lines);
+    y_tracker_mm = 203.0;
+    add_table_header(&current_layer, &oswald, y_tracker_mm);
+    add_order_lines_to_pdf(
+        &current_layer,
+        &normal_roboto,
+        &order.lines,
+        &mut y_tracker_mm,
+    );
+    let total = total_per_order(order);
+    add_total(&current_layer, &medium, &oswald, y_tracker_mm, total);
 
-    doc.save(&mut BufWriter::new(
-        File::create("test_working.pdf").unwrap(),
-    ))
-    .unwrap();
+    let path_name = format!("generated/{}.pdf", order.buyer);
+
+    doc.save(&mut BufWriter::new(File::create(path_name).unwrap()))
+        .unwrap();
 }
 
-fn create_pdf() {
-    let (doc, page1, layer1) =
-        PdfDocument::new("PDF_Document_title", Mm(210.0), Mm(297.0), "Layer 1");
-    let current_layer = doc.get_page(page1).get_layer(layer1);
-
-    let mut y_tracker_mm = 267.0;
-
-    let medium = doc
-        .add_external_font(File::open("assets/fonts/Roboto-Medium.ttf").unwrap())
-        .unwrap();
-    let normal_roboto = doc
-        .add_external_font(File::open("assets/fonts/Roboto-Regular.ttf").unwrap())
-        .unwrap();
-
-    current_layer.begin_text_section();
-
-    current_layer.set_font(&normal_roboto, 46.0);
-    current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
-    current_layer.set_line_height(8.0);
-    current_layer.set_word_spacing(0.0);
-    current_layer.set_character_spacing(0.0);
-
-    current_layer.write_text("INVOICE", &medium);
-
-    current_layer.end_text_section();
-
-    current_layer.begin_text_section();
-    y_tracker_mm -= 18.0;
-    current_layer.set_font(&normal_roboto, 12.0);
-    current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
-    current_layer.set_fill_color(Color::Rgb(Rgb::new(0.0, 0.04, 0.0, None)));
-
-    current_layer.set_line_height(14.0);
-    current_layer.write_text("Invoice Number", &normal_roboto);
-    current_layer.add_line_break();
-    current_layer.set_font(&normal_roboto, 10.0);
-    current_layer.write_text("00001", &normal_roboto);
-
-    current_layer.end_text_section();
-
-    current_layer.begin_text_section();
-    current_layer.set_text_cursor(Mm(70.0), Mm(y_tracker_mm));
-
-    let dt = Utc.with_ymd_and_hms(2024, 2, 20, 0, 0, 0).unwrap();
-
-    current_layer.set_font(&normal_roboto, 12.0);
-    current_layer.write_text("Date of Issue", &normal_roboto);
-    current_layer.add_line_break();
-    current_layer.set_font(&normal_roboto, 10.0);
-    current_layer.write_text(&dt.format("%d/%m/%Y").to_string(), &normal_roboto);
-
-    current_layer.end_text_section();
-
-    struct BuyerDetails {
-        name: String,
-        address1: String,
-        address2: Option<String>,
-        city: String,
-        postcode: String,
-        country: String,
-    }
-
-    let buyer_details = BuyerDetails {
-        name: "Exeter College".to_string(),
-        address1: "Turl Street".to_string(),
-        address2: None,
-        city: "Oxford".to_string(),
-        postcode: "OX1 3DP".to_string(),
-        country: "United Kingdom".to_string(),
-    };
-
-    current_layer.begin_text_section();
-
-    y_tracker_mm -= 16.0;
-    current_layer.set_font(&normal_roboto, 14.0);
-    current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
-    current_layer.write_text("BILLED TO", &normal_roboto);
-
-    current_layer.end_text_section();
-
-    y_tracker_mm -= 7.0;
-
-    current_layer.begin_text_section();
-    current_layer.set_font(&normal_roboto, 10.0);
-    current_layer.set_text_cursor(Mm(10.0), Mm(y_tracker_mm));
-
-    current_layer.set_line_height(12.0);
-    // write two lines (one line break)
-    current_layer.write_text(&buyer_details.name, &normal_roboto);
-    current_layer.add_line_break();
-    current_layer.write_text(&buyer_details.address1, &normal_roboto);
-    current_layer.add_line_break();
-
-    if let Some(address2) = &buyer_details.address2 {
-        current_layer.write_text(address2, &normal_roboto);
-        current_layer.add_line_break();
-    }
-
-    current_layer.write_text(&buyer_details.city, &normal_roboto);
-    current_layer.add_line_break();
-    current_layer.write_text(&buyer_details.postcode, &normal_roboto);
-    current_layer.add_line_break();
-    current_layer.write_text(&buyer_details.country, &normal_roboto);
-
-    current_layer.end_text_section();
-
-    doc.save(&mut BufWriter::new(
-        File::create("test_working.pdf").unwrap(),
-    ))
-    .unwrap();
-}
 
 fn example(path: std::path::PathBuf) -> Result<(), Error> {
     let mut workbook: Xlsx<_> = open_workbook(path)?;
@@ -410,9 +421,20 @@ fn example(path: std::path::PathBuf) -> Result<(), Error> {
     let headers = headers(&mut range);
     let (buyers, buyer_start_col) = get_buyers(&headers);
 
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
+    stdout.set_color(ColorSpec::new().set_fg(Some(termcolor::Color::White)))?;
+    writeln!(&mut stdout, "\nBuyers: ")?;
+    writeln!(&mut stdout, "----------------------")?;
+
     for buyer in &buyers {
-        println!("Buyer: {}", buyer);
+        stdout.set_color(ColorSpec::new().set_fg(Some(termcolor::Color::Cyan)))?;
+        writeln!(&mut stdout, "{}", buyer.name)?;
     }
+
+    println!("\n");
+
+    WriteColor::reset(&mut stdout)?;
 
     let data = RangeDeserializerBuilder::new().from_range(&range).unwrap();
 
@@ -426,7 +448,7 @@ fn example(path: std::path::PathBuf) -> Result<(), Error> {
             buyer.name.clone(),
             Order {
                 buyer: buyer.name.clone(),
-                lines: Vec::new(),
+                lines: std::collections::HashMap::<String, Vec<OrderLine>>::new(),
             },
         );
     }
@@ -461,20 +483,34 @@ fn example(path: std::path::PathBuf) -> Result<(), Error> {
 
         for (i, buyer) in buyers.iter().enumerate() {
             let order = grows[i + buyer_start_col + 1].as_f64().unwrap();
+
+            if order <= 0.0 {
+                continue;
+            }
+
             grower_item_orders.orders.push((&buyer, order as f32));
 
-            buyer_orders
+            let order_line = OrderLine {
+                produce: produce_name.clone(),
+                variant: variant.clone(),
+                unit: unit.clone(),
+                price: price as u32,
+                qty: order as f32,
+            };
+
+            match buyer_orders
                 .get_mut(&buyer.name)
                 .unwrap()
                 .lines
-                .push(OrderLine {
-                    grower: grower.clone(),
-                    produce: produce_name.clone(),
-                    variant: variant.clone(),
-                    unit: unit.clone(),
-                    price: price as u32,
-                    qty: order as f32,
-                });
+                .entry(grower.clone())
+            {
+                std::collections::hash_map::Entry::Vacant(e) => {
+                    e.insert(vec![order_line]);
+                }
+                std::collections::hash_map::Entry::Occupied(mut e) => {
+                    e.get_mut().push(order_line);
+                }
+            }
         }
 
         grower_item_orders_list.push(grower_item_orders);
@@ -482,7 +518,12 @@ fn example(path: std::path::PathBuf) -> Result<(), Error> {
 
     summarise_grower_item_orders(&grower_item_orders_list);
 
-    let orders = Vec::from_iter(buyer_orders.values());
+    print!("{:?}", buyer_orders);
+
+    let orders = Vec::from_iter(buyer_orders.values())
+        .into_iter()
+        .filter(|o| o.lines.len() > 0)
+        .collect::<Vec<&Order>>();
 
     create_buyer_orders(orders);
     Ok(())
