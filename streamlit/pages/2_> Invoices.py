f13a5a8b39de47f12b04d21dc9ffd2d73c556086
Douglas Brierley <douglasbrierley@me.com>
2024-02-28 10:57:04 +0000

    Added start of invoices py

--- streamlit/pages/2_> Invoices.py
+++ streamlit/pages/2_> Invoices.py
@@ -17,104 +17,105 @@ st.title("Invoice Generator")
 
 st.markdown("Upload weekly order excel and contacts CSV to generate delivery notes.")
 
-order_sheet = st.file_uploader("Choose Weekly Order Excel", type="xlsx", accept_multiple_files=False)
-contacts = st.file_uploader("Choose Contacts CSV", type="xlsx", accept_multiple_files=False)
-dataframes = []
+order_sheets = st.file_uploader("Choose Weekly Order Excels", type="xlsx", accept_multiple_files=True)
+contacts = st.file_uploader("Choose Contacts Excel", type="xlsx", accept_multiple_files=False)
 
-if order_sheet and contacts:
-    orders = pd.read_excel(order_sheet, header=2)
-    contacts = pd.read_excel(contacts)
+if order_sheets and contacts:
+    all_orders = []
+    for order_sheet in order_sheets:
+        orders = pd.read_excel(order_sheet, header=2)
+        contacts = pd.read_excel(contacts)
 
-    # Remove rows with no produce name (i.e. nothing listed)
-    orders = orders.dropna(subset=["Produce Name"])
+        # Remove rows with no produce name (i.e. nothing listed)
+        orders = orders.dropna(subset=["Produce Name"])
 
-    # Remove rows where the sum of columns 9 onwards (the buyers area) is equal to 0
-    orders = orders.loc[(orders.iloc[:, 9:].sum(axis=1) != 0)]
+        # Remove rows where the sum of columns 9 onwards (the buyers area) is equal to 0
+        orders = orders.loc[(orders.iloc[:, 9:].sum(axis=1) != 0)]
 
-    # Get the names of the buyers that made orders this week
-    buyers = orders.columns[9:][orders.iloc[:, 9:].sum() > 0].tolist()
+        # Get the names of the buyers that made orders this week
+        buyers = orders.columns[9:][orders.iloc[:, 9:].sum() > 0].tolist()
 
-    if not buyers:
-        print("No buyers this week")
-
-
-    invoice_data = {"orders": []}
-
-    contacts = contacts.rename(
-        columns={
-            "Buyer": "name",
-            "Address Line 1": "address1",
-            "Address Line 2": "adress2",
-            "City": "city",
-            "Postcode": "postcode",
-            "City": "city",
-            "Country": "country",
-        }
-    )
-
-    my_order_lines = []
-
-    orders = orders.rename(
-        columns={
-            "Produce Name": "produce",
-            "Additional Info": "variant",
-            "UNIT": "unit",
-            "Price/   UNIT (£)": "price",
-            "Growers": "seller",
-        }
-    )
-
-    orders["price"] = (orders["price"] * 100).astype(int)
-
-    for buyer in buyers:
-        # Save the rows that are non-zero for the current buyer
-        non_zero_rows = orders.loc[orders[buyer] != 0]
-
-        # Create a new dataframe with the first 8 columns of non_zero_rows and an additional column for orders[buyer]
-        buyer_lines = non_zero_rows.iloc[:, :5].copy()
-        buyer_lines["qty"] = non_zero_rows[buyer]
-        buyer_lines.loc[:, "buyer"] = buyer
-
-        buyer_lines = buyer_lines.to_dict(orient="records")
-        for line in buyer_lines:
-            my_order_lines.append(line)
-
-    buyer_lines = {}
-
-    selective_list = ["produce", "variant", "unit", "price", "qty"]
-
-    for line in my_order_lines:
-        if line["buyer"] not in buyer_lines or not isinstance(buyer_lines[line["buyer"]], dict):
-            buyer_lines[line["buyer"]] = {}
-        if line["seller"] not in buyer_lines[line["buyer"]]:
-            buyer_lines[line["buyer"]][line["seller"]] = []
-        line_without_buyer = {k: v for k, v in line.items() if k not in ["buyer", "seller"]}
-        print("Adding", line_without_buyer, "to", line["buyer"], "from", line["seller"])
-        buyer_lines[line["buyer"]][line["seller"]].append(line_without_buyer)
-        print(buyer_lines[line["buyer"]])
-
-    orders = []
-
-    for buyer, seller_lines in buyer_lines.items():
-        orders.append({"buyer": buyer, "lines": seller_lines})
+        if not buyers:
+            print("No buyers this week")
 
 
-    final_data = {"orders": orders}
+        invoice_data = {"orders": []}
 
-    final_data
-    invoice_data_json = json.dumps(final_data)
+        contacts = contacts.rename(
+            columns={
+                "Buyer": "name",
+                "Address Line 1": "address1",
+                "Address Line 2": "adress2",
+                "City": "city",
+                "Postcode": "postcode",
+                "City": "city",
+                "Country": "country",
+            }
+        )
 
-    Lambda = boto3.client('lambda', region_name="eu-west-2")
-    response = Lambda.invoke(
-        FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:create_orders',
-        InvocationType='RequestResponse',
-        LogType='Tail',
-        # ClientContext='str_jsoning',
-        Payload=invoice_data_json,
-        # Qualifier='string'
-    )
-    result = json.loads(response['Payload'].read().decode('utf-8'))
-    result
+        my_order_lines = []
+
+        orders = orders.rename(
+            columns={
+                "Produce Name": "produce",
+                "Additional Info": "variant",
+                "UNIT": "unit",
+                "Price/   UNIT (£)": "price",
+                "Growers": "seller",
+            }
+        )
+
+        orders["price"] = (orders["price"] * 100).astype(int)
+
+        for buyer in buyers:
+            # Save the rows that are non-zero for the current buyer
+            non_zero_rows = orders.loc[orders[buyer] != 0]
+
+            # Create a new dataframe with the first 8 columns of non_zero_rows and an additional column for orders[buyer]
+            buyer_lines = non_zero_rows.iloc[:, :5].copy()
+            buyer_lines["qty"] = non_zero_rows[buyer]
+            buyer_lines.loc[:, "buyer"] = buyer
+
+            buyer_lines = buyer_lines.to_dict(orient="records")
+            for line in buyer_lines:
+                my_order_lines.append(line)
+
+        buyer_lines = {}
+
+        selective_list = ["produce", "variant", "unit", "price", "qty"]
+
+        for line in my_order_lines:
+            if line["buyer"] not in buyer_lines or not isinstance(buyer_lines[line["buyer"]], dict):
+                buyer_lines[line["buyer"]] = {}
+            if line["seller"] not in buyer_lines[line["buyer"]]:
+                buyer_lines[line["buyer"]][line["seller"]] = []
+            line_without_buyer = {k: v for k, v in line.items() if k not in ["buyer", "seller"]}
+            print("Adding", line_without_buyer, "to", line["buyer"], "from", line["seller"])
+            buyer_lines[line["buyer"]][line["seller"]].append(line_without_buyer)
+            print(buyer_lines[line["buyer"]])
+
+        orders = []
+
+        for buyer, seller_lines in buyer_lines.items():
+            orders.append({"buyer": buyer, "lines": seller_lines})
+
+
+        final_data = {"orders": orders}
+
+        final_data
+        invoice_data_json = json.dumps(final_data)
+
+        Lambda = boto3.client('lambda', region_name="eu-west-2")
+        response = Lambda.invoke(
+            FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:create_orders',
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            # ClientContext='str_jsoning',
+            Payload=invoice_data_json,
+            # Qualifier='string'
+        )
+        result = json.loads(response['Payload'].read().decode('utf-8'))
+        result
 else:
     st.warning("Please upload CSV file(s).")
     st.stop()
