301c476a285e440ba9ea1c174e436b59ae6dbc51
Douglas Brierley <douglasbrierley@me.com>
2024-03-08 23:56:52 +0000

    Added error handling and college names

--- streamlit/pages/1_> Delivery_Notes.py
+++ streamlit/pages/1_> Delivery_Notes.py
@@ -34,76 +34,71 @@ contacts = st.file_uploader("Choose Contacts Excel", type="xlsx", accept_multipl
 date = st.date_input("What's the order date?")
 
 
-if order_sheet and contacts and date:
-    orders = pd.read_excel(order_sheet, header=2)
-    contacts = pd.read_excel(contacts)
-    # Get the names of the buyers that made orders this week
-    buyers = extract_buyer_list(orders)
-    # Format the contacts and orders dataframes
-    contacts = contacts_formatter(contacts)
-    # Check for unmatched buyers
-    unmatched_buyers = contacts_checker(contacts["name"], buyers)
+if st.button("Generate Delivery Notes"):
+    if order_sheet and contacts and date:
+        orders = pd.read_excel(order_sheet, header=2)
+        contacts = pd.read_excel(contacts)
+        # Get the names of the buyers that made orders this week
+        buyers = extract_buyer_list(orders)
+        # Format the contacts and orders dataframes
+        contacts = contacts_formatter(contacts)
+        # Check for unmatched buyers
+        unmatched_buyers = contacts_checker(contacts["key"], buyers)
 
-    orders = orderify(orders)
+        orders = orderify(orders)
 
-    buyer_json_fields = ["name", "address1", "address2", "city", "postcode", "country", "number"]
+        # Write out all the columns we need from the contacts spreadsheet
+        buyer_json_fields = ["name", "address1", "address2", "city", "postcode", "country", "number"]
 
-    order_json_data = []
-    # Iterate through the buyers and create the invoice data
-    for buyer in buyers:
-        # Get the all the buyer's info
-        buyer_info = contacts.loc[contacts["name"] == buyer]
-        # Keep only the fields we need and convert to a dictionary
-        buyer_info = buyer_info[buyer_json_fields].to_dict("records")[0]
-        # Get the lines for the current buyer
-        lines = orders.loc[orders["buyer"] == buyer].drop("buyer", axis=1)
-        lines = lines.to_dict("records")
-        order_json_data.append({
-        "date": date.strftime('%Y-%m-%d'),
-        "buyer": buyer_info,
-        "lines": lines
-        })
+        order_json_data = []
+        # Iterate through the buyers and create the invoice data
+        for buyer in buyers:
+            # Get the all the buyer's info
+            buyer_info = contacts.loc[contacts["key"] == buyer]
+            # Keep only the fields we need and convert to a dictionary
+            buyer_info = buyer_info[buyer_json_fields].to_dict("records")[0]
+            # Get the lines for the current buyer
+            lines = orders.loc[orders["buyer"] == buyer].drop("buyer", axis=1)
+            lines = lines.to_dict("records")
+            order_json_data.append({
+            "date": date.strftime('%Y-%m-%d'),
+            "buyer": buyer_info,
+            "lines": lines
+            })
 
-    buyer_lines = {}
+        final_json_data = {"orders": order_json_data}
+        invoice_data_json = json.dumps(final_json_data)
 
-    selective_list = ["produce", "variant", "unit", "price", "qty"]
+        Lambda = boto3.client('lambda', region_name="eu-west-2")
+        response = Lambda.invoke(
+            FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:create_orders',
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            # ClientContext='str_jsoning',
+            Payload=invoice_data_json,
+            # Qualifier='string'
+            )
+        result = json.loads(response['Payload'].read().decode('utf-8'))
 
-    final_json_data = {"orders": order_json_data}
-
-    invoice_data_json = json.dumps(final_json_data)
-
-    Lambda = boto3.client('lambda', region_name="eu-west-2")
-    response = Lambda.invoke(
-        FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:create_orders',
-        InvocationType='RequestResponse',
-        LogType='Tail',
-        # ClientContext='str_jsoning',
-        Payload=invoice_data_json,
-        # Qualifier='string'
+        i = 0
+        for link in result["links"]:
+            encoded_link = link.replace(" ", "%20")
+            st.markdown(f"[{buyers[i]} Delivery Notes]({encoded_link})")
+            i += 1
+        
+        links_data = {"links": result["links"],
+                    "name": f"{date.strftime('%Y-%m-%d')} Delivery Notes"}
+        links_json = json.dumps(links_data)
+        zip = Lambda.invoke(
+            FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:zipper',
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            # ClientContext='str_jsoning',
+            Payload=links_json,
+            # Qualifier='string'      
         )
-    result = json.loads(response['Payload'].read().decode('utf-8'))
-
-    i = 0
-    for link in result["links"]:
-        encoded_link = link.replace(" ", "%20")
-        st.markdown(f"[{buyers[i]} Delivery Notes]({encoded_link})")
-        i += 1
-    
-    links_data = {"links": result["links"],
-                "name": f"{date.strftime('%Y-%m-%d')} Delivery Notes"}
-    links_json = json.dumps(links_data)
-    zip = Lambda.invoke(
-        FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:zipper',
-        InvocationType='RequestResponse',
-        LogType='Tail',
-        # ClientContext='str_jsoning',
-        Payload=links_json,
-        # Qualifier='string'      
-    )
-    zip = json.loads(zip['Payload'].read().decode('utf-8'))
-    encoded_link = zip["zip"].replace(" ", "%20")
-    st.link_button("Download All Notes", encoded_link)
-
+        zip = json.loads(zip['Payload'].read().decode('utf-8'))
+        encoded_link = zip["zip"].replace(" ", "%20")
+        st.link_button("Download All Notes", encoded_link)
 else:
-    st.warning("Please upload Excel files and select a date.")
-    st.stop()
+    st.warning("Please upload weekly order spreadsheet and contacts spreadsheet and select a date.")
--- streamlit/pages/2_> Invoices.py
+++ streamlit/pages/2_> Invoices.py
@@ -34,104 +34,105 @@ date = st.date_input("What's the Invoice date?")
 # contacts = "example_data/FarmToFork_Invoice_Contacts.xlsx"
 # order_sheets = ["example_data/OxFarmToFork spreadsheet week 7 - 12_02_2024.xlsx", "example_data/OxFarmToFork spreadsheet week 9 - 26_02_2024.xlsx"]
 
-if order_sheets and contacts and date:
-    contacts = pd.read_excel(contacts, sheet_name="Contacts")
-    contacts = contacts_formatter(contacts)
+if st.button("Generate Invoices"):
+    if order_sheets and contacts and date:
+        contacts = pd.read_excel(contacts, sheet_name="Contacts")
+        contacts = contacts_formatter(contacts)
 
-    # vat = pd.read_excel(contacts, sheet_name="VAT")
+        # vat = pd.read_excel(contacts, sheet_name="VAT")
 
-    # Top of the invoice info
-    month = (date - timedelta(days=14)).strftime("%b") # Get the shortened month of the invoice
-    reference = f"F2F-{month}"
-    payment_terms = 14
-    due_date = (date + timedelta(days=payment_terms)).strftime("%Y-%m-%d")
+        # Top of the invoice info
+        month = (date - timedelta(days=14)).strftime("%b") # Get the shortened month of the invoice
+        reference = f"F2F-{month}"
+        payment_terms = 14
+        due_date = (date + timedelta(days=payment_terms)).strftime("%Y-%m-%d")
 
-    invoice_data = []
-    all_orders = pd.DataFrame()
-    buyers = set()
+        invoice_data = []
+        all_orders = pd.DataFrame()
+        buyers = set()
 
-    # Iterate through the order sheets, adding the orders to the all_orders dataframe
-    for order_sheet in order_sheets:
-        marketplace = pd.read_excel(order_sheet, sheet_name="GROWERS' PAGE", header=2)
-        order_date = date_extractor(order_sheet)
+        # Iterate through the order sheets, adding the orders to the all_orders dataframe
+        for order_sheet in order_sheets:
+            marketplace = pd.read_excel(order_sheet, sheet_name="GROWERS' PAGE", header=2)
+            order_date = date_extractor(order_sheet)
+            
+            # Get the names of the buyers that made orders this week, using the "Buyers:" column as a marker
+            buyers_column_index = marketplace.columns.get_loc("BUYERS:")
+            new_buyers = marketplace.columns[buyers_column_index + 1:][marketplace.iloc[:, buyers_column_index + 1:].sum() > 0].tolist()
+            buyers.update(new_buyers)  # Update buyers with new_buyers
+
+            if not new_buyers:
+                st.write("No buyers this week")
+                continue
+
+            orders = orderify(marketplace)
+
+            # Make the delivery date 8 days after the order date
+            orders["date"] = (order_date + timedelta(days=8)).strftime('%Y-%m-%d')
+            all_orders = pd.concat([all_orders, orders], ignore_index=True)
         
-        # Get the names of the buyers that made orders this week, using the "Buyers:" column as a marker
-        buyers_column_index = marketplace.columns.get_loc("BUYERS:")
-        new_buyers = marketplace.columns[buyers_column_index + 1:][marketplace.iloc[:, buyers_column_index + 1:].sum() > 0].tolist()
-        buyers.update(new_buyers)  # Update buyers with new_buyers
+        # Add the VAT rate to the orders
+        all_orders["vat_rate"] = 0
 
-        if not new_buyers:
-            st.write("No buyers this week")
-            continue
+        # Add the item column to the orders
+        all_orders["item"] = all_orders["produce"] + " - " + all_orders["variant"]
+        columns_to_drop = ["unit", "produce", "variant"]
+        all_orders = all_orders.drop(columns_to_drop, axis=1)
 
-        orders = orderify(marketplace)
-        # Make changes to match the invoice data structure
-        orders["date"] = (order_date + timedelta(days=8)).strftime('%Y-%m-%d')
-        all_orders = pd.concat([all_orders, orders], ignore_index=True)
-    
-    # Add the VAT rate to the orders
-    all_orders["vat_rate"] = 0.2
+        # Check for unmatched buyers
+        unmatched_buyers = contacts_checker(contacts["key"], buyers)
 
-    # Add the item column to the orders
-    all_orders["item"] = all_orders["produce"] + " - " + all_orders["variant"]
-    columns_to_drop = ["unit", "produce", "variant"]
-    all_orders = all_orders.drop(columns_to_drop, axis=1)
+        # Iterate through the buyers and create the invoice data
+        for buyer in buyers:
+            buyer_info = contacts.loc[contacts["key"] == buyer].to_dict("records")[0]
+            lines = all_orders.loc[all_orders["buyer"] == buyer].drop("buyer", axis=1)
+            lines = lines.to_dict("records")
+            invoice_data.append({
+            "date": date.strftime('%Y-%m-%d'),
+            "due_date": due_date,  
+            "reference": reference,
+            "buyer": buyer_info,
+            "lines": lines
+            })
 
-    # Check for unmatched buyers
-    unmatched_buyers = contacts_checker(contacts["name"], buyers)
-
-    # Iterate through the buyers and create the invoice data
-    for buyer in buyers:
-        buyer_info = contacts.loc[contacts["name"] == buyer].to_dict("records")[0]
-        lines = all_orders.loc[all_orders["buyer"] == buyer].drop("buyer", axis=1)
-        lines = lines.to_dict("records")
-        invoice_data.append({
-        "date": date.strftime('%Y-%m-%d'),
-        "due_date": due_date,  # Add 14 days to the date
-        "reference": reference,
-        "buyer": buyer_info,
-        "lines": lines
-        })
-
-    final_data = {"invoices": invoice_data}
-    invoice_data_json = json.dumps(final_data)
+        final_data = {"invoices": invoice_data}
+        invoice_data_json = json.dumps(final_data)
 
 
-    Lambda = boto3.client('lambda', region_name="eu-west-2")
-    response = Lambda.invoke(
-        FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:create_invoices',
-        InvocationType='RequestResponse',
-        LogType='Tail',
-        # ClientContext='str_jsoning',
-        Payload=invoice_data_json,
-        # Qualifier='string'
-    )
-    result = json.loads(response['Payload'].read().decode('utf-8'))
-    
-    i = 0
-    buyers = list(buyers)
-    for link in result["links"]:
-        encoded_link = link.replace(" ", "%20")
-        st.markdown(f"[{buyers[i]} Invoice]({encoded_link})")
-        i += 1
-
-    links_data = {"links": result["links"],
-        "name": f"{date.strftime('%Y-%m-%d')} Invoice"}
+        Lambda = boto3.client('lambda', region_name="eu-west-2")
+        response = Lambda.invoke(
+            FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:create_invoices',
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            # ClientContext='str_jsoning',
+            Payload=invoice_data_json,
+            # Qualifier='string'
+        )
+        result = json.loads(response['Payload'].read().decode('utf-8'))
         
-    links_json = json.dumps(links_data)
-    zip = Lambda.invoke(
-        FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:zipper',
-        InvocationType='RequestResponse',
-        LogType='Tail',
-        # ClientContext='str_jsoning',
-        Payload=links_json,
-        # Qualifier='string'      
-    )
-    zip = json.loads(zip['Payload'].read().decode('utf-8'))
-    encoded_link = zip["zip"].replace(" ", "%20")
-    st.link_button("Download All Notes", encoded_link)
+        i = 0
+        buyers = list(buyers)
+        for link in result["links"]:
+            encoded_link = link.replace(" ", "%20")
+            st.markdown(f"[{buyers[i]} Invoice]({encoded_link})")
+            i += 1
+
+        links_data = {"links": result["links"],
+            "name": f"{date.strftime('%Y-%m-%d')} Invoice"}
+            
+        links_json = json.dumps(links_data)
+        zip = Lambda.invoke(
+            FunctionName='arn:aws:lambda:eu-west-2:850434255294:function:zipper',
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            # ClientContext='str_jsoning',
+            Payload=links_json,
+            # Qualifier='string'      
+        )
+        zip = json.loads(zip['Payload'].read().decode('utf-8'))
+        encoded_link = zip["zip"].replace(" ", "%20")
+        st.link_button("Download All Invoices", encoded_link)
 
 else:
-    st.warning("Please upload Excel file(s).")
+    st.warning("Please upload weekly order spreadsheet and contacts spreadsheet and select a date.")
     st.stop()
-
