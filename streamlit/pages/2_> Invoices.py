aa5fa3b8955d2a1f483d5768f939b716a688ff57
Douglas Brierley <douglasbrierley@me.com>
2024-04-08 23:24:12 +0100

    Added to invoice instructions

--- streamlit/pages/2_> Invoices.py
+++ streamlit/pages/2_> Invoices.py
@@ -28,6 +28,7 @@ st.title("Invoice Generator")
 instructions = '''
 1. Download all the weekly order Excels from the weekly links
 2. Rename the Excel to the format: OxFarmToFork spreadsheet week N - DD_MM_YYYY.xlsx
+    - Where N is the week number and DD_MM_YYYY is the Monday of the order week
 3. Update the contacts spreadsheet with all contact info.
     Note:
     - Do not change the column titles
