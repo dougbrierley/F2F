68b3947fc645354f956f61b447157aa27866fac4
Douglas Brierley <douglasbrierley@me.com>
2024-04-19 12:24:21 +0100

    Changed home copy

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,10 +1,11 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="Home")
 
-st.write("# Farm To Fork Invoice and Delivery Notes Generator")
-st.markdown("Welcome to the Farm To Fork Invoice and Delivery Notes Generator.")
+st.write("# OxFarmToFork Invoice and Delivery Notes Generator")
+st.markdown("Welcome to the OxFarmToFork Invoice and Delivery Notes Generator.")
 st.markdown("Please select a generator from the sidebar to get started.")
 
 st.sidebar.success("Select a generator above.")
