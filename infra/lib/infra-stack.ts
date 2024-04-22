e51d1238a8700a5d605b8a7d13935325dd3e3ddf
Fraser Rennie <fjrennie1@outlook.com>
2024-04-23 00:21:16 +0100

    infrastructure basic app ready

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,68 +1,110 @@
 import * as cdk from "aws-cdk-lib";
-import { Port, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
-import { Repository } from "aws-cdk-lib/aws-ecr";
-import { Cluster, EcrImage, FargateService, FargateTaskDefinition } from "aws-cdk-lib/aws-ecs";
-import { ApplicationLoadBalancer } from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { Certificate, CertificateValidation } from "aws-cdk-lib/aws-certificatemanager";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
+import {
+  AwsLogDriver,
+  Cluster,
+  ContainerImage,
+  FargateService,
+  FargateTaskDefinition,
+} from "aws-cdk-lib/aws-ecs";
+import {
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  ListenerAction,
+  ListenerCertificate,
+  SslPolicy,
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { ARecord, CnameRecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
 import { LoadBalancerTarget } from "aws-cdk-lib/aws-route53-targets";
 import { Construct } from "constructs";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const domainName = "oxfarmtofork.org";
 
-    const vpc = new Vpc(this, "InfraVpc", { maxAzs: 2 });
-
-    const cluster = new Cluster(this, "Ec2Cluster", { vpc });
-
-    const repo = new Repository(this, "farmtoforkstreamlitrepo");
-    const image = new EcrImage(repo, "farmtoforkstreamlitapp");
-
-    const taskDefinition = new FargateTaskDefinition(this, "StreamlitTaskDefinition");
-    taskDefinition.addContainer("streamlitcontainer", {
-      image: image,
-      portMappings: [{ containerPort: 8080 }]
+    const vpc = new Vpc(this, "InfraVpc", {
+      maxAzs: 2,
     })
 
-    const hostedZone = HostedZone.fromLookup(this, 'oxfarmtofork.org', {
-      domainName: 'oxfarmtofork.org'
+    const cluster = new Cluster(this, "InfraCluster", {
+      vpc: vpc,
+      clusterName: "InfraCluster",
+      containerInsights: true,
     });
 
-    const loadBalancer = new ApplicationLoadBalancer(this, "LoadBalancer", {
-      vpc,
+    const image = ContainerImage.fromRegistry("amazon/amazon-ecs-sample");
+
+    const loadBalancer = new ApplicationLoadBalancer(this, "InfraLoadBalancer", {
+      vpc: vpc,
       internetFacing: true,
+      idleTimeout: cdk.Duration.seconds(120),
     });
 
-    const albAlias = new LoadBalancerTarget(loadBalancer);
-    new ARecord(this, 'ARecord', {
-        zone: hostedZone,
-        target: RecordTarget.fromAlias(albAlias),
+    const hostedZone = HostedZone.fromLookup(this, "InfraHostedZone", {
+      domainName,
     });
 
-    const service = new FargateService(this, "StreamlitService", {
-      cluster,
-      taskDefinition,
-      desiredCount: 1,
-      vpcSubnets: {
-        subnetType: SubnetType.PUBLIC
-      },
+    const certificate = new Certificate(this, "InfraCertificate", {
+      domainName,
+      validation: CertificateValidation.fromDns(hostedZone),
     });
 
-    service.connections.allowFrom(
-      loadBalancer,
-      Port.tcp(8080),
-      "Allow inbound traffic from ALB"
-    )
+    const listener = loadBalancer.addListener("InfraListener", {
+      protocol: ApplicationProtocol.HTTPS,
+      sslPolicy: SslPolicy.RECOMMENDED,
+    })
 
-    const Listener = loadBalancer.addListener("Listener", {
+    const targetGroup = listener.addTargets("InfraTargetGroup", {
+      protocol: ApplicationProtocol.HTTP,
+    });
+
+    listener.addCertificates("InfraCertificate", [ListenerCertificate.fromCertificateManager(certificate)]);
+
+    loadBalancer.addListener('PublicRedirectListener', {
+      protocol: ApplicationProtocol.HTTP,
       port: 80,
       open: true,
+      defaultAction: ListenerAction.redirect({
+        port: '443',
+        protocol: ApplicationProtocol.HTTPS,
+        permanent: true,
+      }),
+    });
+    
+    new ARecord(this, "InfraCnameRecord", {
+      zone: hostedZone,
+      recordName: domainName,
+      target: RecordTarget.fromAlias(new LoadBalancerTarget(loadBalancer))
     });
 
+    new cdk.CfnOutput(this, "InfraLoadBalancerDNS", { value: loadBalancer.loadBalancerDnsName });
+    new cdk.CfnOutput(this, "ServiceURL", { value: "https://" +  domainName});
 
-    Listener.addTargets("ECS", {
-      port: 8080,
-      targets: [service],
+    const fargateTaskDefinition = new FargateTaskDefinition(this, "InfraTaskDefinition", {
+      memoryLimitMiB: 2048,
+      cpu: 1024,
     });
+
+    const container = fargateTaskDefinition.addContainer("InfraContainer", {
+      image,
+      logging: new AwsLogDriver({ streamPrefix: id }),
+    });
+
+    container.addPortMappings({
+      containerPort: 80,
+    });
+
+    const service = new FargateService(this, "InfraService", {
+      cluster,
+      taskDefinition: fargateTaskDefinition,
+      desiredCount: 1,
+      assignPublicIp: true,
+    });
+
+    targetGroup.addTarget(service);
+
   }
 }
