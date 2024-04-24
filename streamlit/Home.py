d50c3edb708d66eefa3e98aa6e23cf8e5b7a0508
Fraser Rennie <fjrennie1@outlook.com>
2024-04-24 23:13:19 +0100

    trivial change for test

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -6,7 +6,7 @@ st.set_page_config(
 
 st.write("# OxFarmToFork Invoice and Delivery Notes Generator")
 st.markdown("Welcome to the OxFarmToFork Invoice and Delivery Notes Generator.")
-st.markdown("Please select a generator from the sidebar to get started!")
+st.markdown("Please select a generator from the sidebar to get started.")
 
 st.sidebar.success("Select a generator above.")
 
