13c56217d6a15223c3725c9a1980233cf510dfd9
Douglas Brierley <douglasbrierley@me.com>
2024-07-25 14:24:21 +0100

    pick list summary

--- streamlit/create_pick_lists.py
+++ streamlit/create_pick_lists.py
@@ -5,7 +5,7 @@ from domain import MarketPlace, PickList
 
 
 def create_pick_lists(
-    market_place: MarketPlace, monday_of_order_week: date, week_number: int
+    market_place: MarketPlace, monday_of_order_week: date, week_number: int, include_summary: bool = False
 ) -> list[PickList]:
     """
     Create the pick lists for the sellers
@@ -24,6 +24,33 @@ def create_pick_lists(
             i -= 1
             continue
 
+        if include_summary:
+            temp_dict : dict[str, list[Order]] = {}
+            no_price_items : list[Order] = []
+            buyer=Buyer("Total", "", "", "", "", "", None)
+            # Group the items by unique produce
+            for order in orders:
+                key=f"{order.produce}-{order.variant}"
+                if key not in temp_dict:
+                    temp_dict[key] = []
+                temp_dict[key].append(order)
+            # Sum the quanitity across the list of orders
+            for key, value in temp_dict.items():
+                quantity = 0
+                for order in value:
+                    quantity += order.quantity
+                # Deep copy
+                no_price=Order(
+                    produce=order.produce,
+                    unit=order.unit,
+                    seller=order.seller,
+                    variant=order.variant,
+                    price=0,
+                    buyer=buyer,
+                    quantity=quantity
+                )
+                no_price_items.append(no_price)
+        orders.extend(no_price_items)
         if orders:
             all_pick_lists.append(
                 PickList(
