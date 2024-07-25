9d33096fda9e6eee7e6d9bef833ad1480d2f6257
Douglas A W Brierley <77011389+dougbrierley@users.noreply.github.com>
2024-07-25 14:35:47 +0100

    Bug/order summary fix and add pick list totals (#79)
    * Bug fix
    
    * Added pick list summary
    
    * pick list summary
    
    * Formatting
    
    * Added imports
    
    * Put totals in pick list at the bottom
    
    * Removed print in delivery notes

--- streamlit/create_pick_lists.py
+++ streamlit/create_pick_lists.py
@@ -1,11 +1,11 @@
 "Create the pick lists for the sellers"
 
 from datetime import date
-from domain import MarketPlace, PickList
+from domain import MarketPlace, PickList, Buyer, Order
 
 
 def create_pick_lists(
-    market_place: MarketPlace, monday_of_order_week: date, week_number: int
+    market_place: MarketPlace, monday_of_order_week: date, week_number: int, include_summary: bool = False
 ) -> list[PickList]:
     """
     Create the pick lists for the sellers
@@ -24,12 +24,39 @@ def create_pick_lists(
             i -= 1
             continue
 
+        if include_summary:
+            temp_dict : dict[str, list[Order]] = {}
+            no_price_items : list[Order] = []
+            buyer=Buyer("Total", "", "", "", "", "", None)
+            # Group the items by unique produce
+            for order in orders:
+                key=f"{order.produce}-{order.variant}"
+                if key not in temp_dict:
+                    temp_dict[key] = []
+                temp_dict[key].append(order)
+            # Sum the quanitity across the list of orders
+            for key, value in temp_dict.items():
+                quantity = 0
+                for order in value:
+                    quantity += order.quantity
+                # Deep copy
+                no_price=Order(
+                    produce=order.produce,
+                    unit=order.unit,
+                    seller=order.seller,
+                    variant=order.variant,
+                    price=0,
+                    buyer=buyer,
+                    quantity=quantity
+                )
+                no_price_items.append(no_price)
+        orders.extend(no_price_items)
         if orders:
             all_pick_lists.append(
                 PickList(
                     monday_of_order_week=monday_of_order_week,
                     seller=seller,
-                    orders=frozenset(orders),
+                    orders=list(orders),
                     reference=f"F2FP{week_number}{monday_of_order_week.strftime('%Y')[2:4]}{i}",
                 )
             )
