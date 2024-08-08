6c5b3051633fa2c11c79c96be429754618b74058
Douglas Brierley <douglasbrierley@me.com>
2024-08-08 18:03:02 +0100

    Fixed totals duplication

--- streamlit/create_pick_lists.py
+++ streamlit/create_pick_lists.py
@@ -1,11 +1,10 @@
 "Create the pick lists for the sellers"
 
 from datetime import date
-from domain import MarketPlace, PickList, Buyer, Order
+from domain import MarketPlace, PickList
 
 def create_pick_lists(
-    market_place: MarketPlace, monday_of_order_week: date, week_number: int, include_summary=False
-) -> list[PickList]:
+    market_place: MarketPlace, monday_of_order_week: date, week_number: int) -> list[PickList]:
     """
     Create the pick lists for the sellers
     """
@@ -22,34 +21,6 @@ def create_pick_lists(
         if len(orders) == 0:
             i -= 1
             continue
-
-        if include_summary:
-            temp_dict : dict[str, list[Order]] = {}
-            no_price_items : list[Order] = []
-            buyer=Buyer("Total", "", "", "", "", "", None)
-            # Group the items by unique produce
-            for order in orders:
-                key=f"{order.produce}-{order.variant}"
-                if key not in temp_dict:
-                    temp_dict[key] = []
-                temp_dict[key].append(order)
-            # Sum the quanitity across the list of orders
-            for key, value in temp_dict.items():
-                quantity = 0
-                for order in value:
-                    quantity += order.quantity
-                # Deep copy
-                no_price=Order(
-                    produce=order.produce,
-                    unit=order.unit,
-                    seller=order.seller,
-                    variant=order.variant,
-                    price=0,
-                    buyer=buyer,
-                    quantity=quantity
-                )
-                no_price_items.append(no_price)
-            orders.extend(no_price_items)
 
         if orders:
             all_pick_lists.append(
